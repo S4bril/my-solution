93ba2bf865ad573949a72d71cc755eb5652fe76d
S4bril <95976169+S4bril@users.noreply.github.com>
2026-05-19 19:49:43 +0200

    Fix created_at to consent_date in candidate table

--- new-recruitment-api/migrations/004-create-candidate.sql
+++ new-recruitment-api/migrations/004-create-candidate.sql
@@ -6,5 +6,5 @@ CREATE TABLE Candidate (
     years_of_experience INTEGER,
     recruiter_notes TEXT,
     recruitment_status TEXT CHECK(recruitment_status IN ('new', 'interviewing', 'accepted', 'rejected')) NOT NULL DEFAULT 'new',
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    consent_date DATETIME DEFAULT CURRENT_TIMESTAMP
 );
